79ce2e3a5c94edcb6ab6c0f080e28394a9c3328c
std138170 <56333691+std138170@users.noreply.github.com>
2020-06-21 21:26:15 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -182,7 +182,7 @@ function addPoints(data) {
     marker.addTo(pointGroupLayer);
 
     // UNCOMMENT THIS LINE TO USE POPUPS
-    marker.bindPopup('<h2>' + data[row].location + '</h2>There's a ' + data[row].level + ' ' + data[row].category + ' here');
+   // marker.bindPopup('<h2>' + data[row].location + '</h2>There's a ' + data[row].level + ' ' + data[row].category + ' here');
 
     // COMMENT THE NEXT 14 LINES TO DISABLE SIDEBAR FOR THE MARKERS
     marker.feature = {
