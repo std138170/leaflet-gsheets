fcea854ba23158a3576f7c7fa92e431033df6044
std138170 <56333691+std138170@users.noreply.github.com>
2020-06-21 16:51:12 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -171,7 +171,11 @@ function addPoints(data) {
     } else if (markerType == "circle") {
       marker = L.circle([data[row].lat, data[row].lon], {radius: markerRadius});
     } else {
-      marker = L.marker([data[row].lat, data[row].lon], {icon: myIcon});
+	    var distance = getDistance([data[row].lat, data[row].lon], [mycoordinates.lat, mycoordinates.lon]);
+	    if (distance < 1 )
+	    {
+     		 marker = L.marker([data[row].lat, data[row].lon]);
+	    }
     }
     marker.addTo(pointGroupLayer);
 
@@ -276,5 +280,24 @@ var overlays =
 	};
                 L.control.layers(overlays, null).addTo(map);
 
+ function getDistance(origin, destination) 
+    {
+        var lon1 = toRadian(origin[1]),
+        lat1 = toRadian(origin[0]),
+        lon2 = toRadian(destination[1]),
+        lat2 = toRadian(destination[0]);
+
+        var deltaLat = lat2 - lat1;
+        var deltaLon = lon2 - lon1;
+
+        var a = Math.pow(Math.sin(deltaLat/2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(deltaLon/2), 2);
+        var c = 2 * Math.asin(Math.sqrt(a));
+        var EARTH_RADIUS = 6371;
+        return c * EARTH_RADIUS * 1000;
+    }
+function toRadian(degree)
+    {
+        return degree*Math.PI/180;
+    }
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
