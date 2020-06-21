a84b1e4932782ab0707a626ac1301a96a71a14b7
std138170 <56333691+std138170@users.noreply.github.com>
2020-06-21 15:55:15 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -249,6 +249,7 @@ var myIcon = L.icon({
 
 var Colosseum  = L.marker([41.890209, 12.492231], {icon: myIcon}).bindPopup("Κολοσαίο!");
 Colosseum.addTo(map);
+L.marker([51.5, -0.09], {icon: myIcon}).addTo(map);
 
 map.on('locationfound', onLocationFound);
 
