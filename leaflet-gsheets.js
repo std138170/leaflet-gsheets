6e049f16f14bbcc2e4165fa4fd81d871217d8416
std138170 <56333691+std138170@users.noreply.github.com>
2020-06-24 20:07:19 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -242,7 +242,7 @@ function onLocationFound(e) {
   mycoordinates = e.latlng;
 
     L.marker(e.latlng).addTo(map)
-        .bindPopup("Your coordinates are!!! " + mycoordinates.lat).openPopup();
+        .bindPopup("Your coordinates are " + mycoordinates.lat).openPopup();
 
     L.circle(e.latlng, radius).addTo(map);
 }
