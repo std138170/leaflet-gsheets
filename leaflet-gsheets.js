fc0783ec116860cd70315581e09f6bb1622add1a
std138170 <56333691+std138170@users.noreply.github.com>
2020-07-04 18:24:27 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -216,33 +216,3 @@ function getColor(type) {
   }
 }
 
-//*************************************************************************************
-
- function getDistance(origin, destination) 
-    {
-
-	//return distance in meters
-        var lon1 = toRadian(origin[1]),
-        lat1 = toRadian(origin[0]),
-        lon2 = toRadian(destination[1]),
-        lat2 = toRadian(destination[0]);
-	    
-		alert("Μεσα στη συναρτηση lon1 " +lon1);
-	    alert("Μεσα στη συναρτηση lon2 " +lon1);
-	    alert("Μεσα στη συναρτηση lat1 " +lat1);
-	    alert("Μεσα στη συναρτηση lat2 " +lat2);
-        var deltaLat = lat2 - lat1;
-        var deltaLon = lon2 - lon1;
-		alert("Μεσα στη συναρτηση deltaLat   " +deltaLat);
-	    alert("Μεσα στη συναρτηση deltaLon  " +deltaLon);
-	    
-        var a = Math.pow(Math.sin(deltaLat/2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(deltaLon/2), 2);
-        var c = 2 * Math.asin(Math.sqrt(a));
-        var EARTH_RADIUS = 6371;
-        return c * EARTH_RADIUS * 1000;
-    }
-function toRadian(degree)
-    {
-        return degree*Math.PI/180;
-    }
-  alert("1");
